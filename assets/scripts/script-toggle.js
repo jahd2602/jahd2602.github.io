55061a78d727734912db6b7fb4a8dad40e5451f1
Karen <karenbanci1@gmail.com>
2022-12-18 21:48:45 -0800

    ajust size toggle and styles

--- assets/scripts/script-toggle.js
+++ assets/scripts/script-toggle.js
@@ -1,19 +1,11 @@
 var btn_toggle = document.getElementsByClassName("btn-toggle");
 var menu_toggle = document.getElementsByClassName("menu-toggle");
 
-// document.getElementsByClassName("toggle").addEventListener("click", function (e) {
-//   btn_toggle.style.display = "block";
-// });
-
-// document.getElementsByClassName("hero").addEventListener("click", function (e) {
-//   btn_toggle.style.display = "none";
-// });
-
 window.toggleOn = function (this_element, event) {
   event.stopPropagation();
   console.log("🚀 ~ file: script-toggle.js:13 onnnnn", this_element)
 
-  menu_toggle[0].style.display = "block";
+  menu_toggle[0].style.display = "flex";
   btn_toggle[0].style.display = "none";
 }
 
@@ -21,5 +13,5 @@ window.toggleOff = function () {
   console.log("🚀 ~ file: script-toggle.js:20 ~ toggleOff", toggleOff)
 
   menu_toggle[0].style.display = "none";
-  btn_toggle[0].style.display = "block";
+  btn_toggle[0].style.display = "flex";
 }
